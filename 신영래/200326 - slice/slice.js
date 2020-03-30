660f2cb5e60eca6f8a21b5949f63c93b10436efc
young-rae Shin <lived1024@gmail.com>
2020-03-31 01:27:34 +0900

    풀이 완료

--- 신영래/200326 - slice/slice.js
+++ 신영래/200326 - slice/slice.js
@@ -1,37 +1,100 @@
-var all = [];           // 전체 숫자를 담는 배열
+var allArr = [];           // 전체 숫자를 담는 배열
 var resultArr = [];     // 2중 배열로 값을 저장
-
-// 전체 숫자 담기
-function ready(no)
-{
-    for(var i = 0; i < 20; i++)
-    {
-        all[i] = i + 1;
-    }
-}
 
 // 작동
 function run(no, person)
 {
     ready(no);
-    start(no, person);
+    calCount();
+    console.log(allArr);
+    console.log("--------------------------------");
+    console.log(resultArr);
+    console.log("--------------------------------");
+    action(person);
+
 }
 
-function start(no, person)
+// 무조건 절반으로 쪼갠 후 배열 저장
+function ready(no)
 {
+    var cnt = 0;
+    while(true)
+    {
+        var newArr = new Array();
+        if(cnt == 0)
+        {
+            allArr[cnt] = [no];
 
+            var fir = Math.ceil( allArr[cnt] / 2 );
+            var sec = allArr[cnt] - fir;
+            newArr.push(fir);
+            newArr.push(sec);
+        }
+        else 
+        {
+            var arr = allArr[cnt];
+            for(var i = 0; i < arr.length; i++)
+            {
+                if(arr[i] >= 2)
+                {
+                    var fir = Math.ceil( arr[i] / 2 );
+                    var sec = arr[i] - fir;
+                    newArr.push(fir, sec);
+                }
+                else
+                {
+                    newArr.push(arr[i]);
+                }
+            }
+        }
 
-    slice(no, targetArr);
+        allArr[cnt+1] = newArr;
+        if(newArr.every(allCheck)) break; 
+        cnt++;
+    }
 }
 
-// 반으로 쪼갠다!
-function slice(no, targetArr)
+function allCheck(element, index, array) 
 {
-    var mid = Math.ceil(no / 2) ;
-    var firSliceArr = targetArr.slice(0, mid);
-    var secSliceArr = targetArr.slice(mid, targetArr.length);
-
-    
+    return element == 1;
 }
 
-run(20);
+function calCount()
+{
+    for(var i = 0 ; i < allArr.length ; i++)
+    {
+        var value = allArr[i];
+        // 2이상인 묶음의 개수
+        var cnt = 0;
+        // 반복문으로 한 묶음에 2개 이상인 것을 센다.
+        for(var val of value)
+        {
+            if(val >= 2)
+            {
+                cnt++;
+            }
+        }
+        resultArr.push(cnt);
+    }
+}
+
+function action(person)
+{
+    var result = 0;
+    for(var i = 0; i < resultArr.length; i++)
+    {
+        if(i <= 1 )
+        {
+            result += Math.ceil( resultArr[i] / person );
+        }else
+        {
+            result += ( resultArr[i] / person );
+        }
+        console.log((i+1) + "번째 : " + result);
+    }
+    result = Math.ceil(result);
+    console.log(result + "번");
+}
+
+run(20, 3);
+//run(100, 5);
